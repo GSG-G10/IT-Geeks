da8b65fcac65708c409922c70d6101413c419519
Ahmad Omar <ahmad@pop-os.localdomain>
2021-08-24 20:42:28 +0300

    hanlde displayData and addData endpoints Relates 25

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const { errorNotFound, errorServer } = require('./error');
+const displayData = require('./displayData');
+const addData = require('./addData');
+
+router.get('/displayData', displayData);
+router.post('/addData', addData);
 
 router.use(errorNotFound);
 router.use(errorServer);
