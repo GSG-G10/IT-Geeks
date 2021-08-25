49d0ce4bda93da69d80ebdbfa62d1cf808a28376
Ahmad Omar <ahmad@pop-os.localdomain>
2021-08-25 16:39:12 +0300

    hash password before insert to table Relates #32

--- src/controllers/signupData.js
+++ src/controllers/signupData.js
@@ -1,12 +1,14 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-console */
+const bcrypt = require('bcryptjs');
 const { signupQuery } = require('../database/queries');
 
 const signupData = (req, res) => {
   const {
     first_name, last_name, email_address, phono_no, password,
   } = req.body;
-  signupQuery(first_name, last_name, email_address, phono_no, password)
+  const hash = bcrypt.hash(password, 10);
+  hash.then((hashed) => signupQuery(first_name, last_name, email_address, phono_no, hashed))
     .then(() => res.redirect('/'))
     .catch((err) => console.log(err));
 };
