f117335e37ae26da60483e0900fbd241515e3770
Ahmad Omar <ahmad@pop-os.localdomain>
2021-08-26 14:27:08 +0300

    handle logout button in dashboard Relates #43

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -11,6 +11,17 @@ router.get('/displayData', displayData);
 router.post('/addData', addData);
 router.post('/signup', signupData);
 router.post('/login', loginData);
+router.use('/logout', (req, res) => {
+  res.clearCookie('token').clearCookie('login').redirect('/');
+});
+router.get('/dashboard', (req, res, next) => {
+  if (req.cookies.login) {
+    next();
+    res.json({ msg: 'welcome to your dashboard' });
+  } else {
+    res.json({ msg: 'sorry! you are not authorized' });
+  }
+});
 
 router.use(errorNotFound);
 router.use(errorServer);
